a511936f16d3f22ff106f632a06bd1787f7434c7
PhilippAusgust <s7phmuen@uni-bonn.de>
2022-11-20 00:11:38 +0100

    update apply

--- Apply_october_2022.R
+++ Apply_october_2022.R
@@ -12,16 +12,39 @@ Langenfeld  = universal_function("Oktober_2022_Langenfeld",
                                             enddate = "2022-11-01")[[2]]
 
 
+# preparation for min_max
+
+Kalenborn_val = universal_function("Oktober_2022_Kalenborn",
+                               pivot = TRUE,
+                               startdate = "2022-09-30",
+                               enddate = "2022-11-01")[[1]]
+
+Langenfeld_val  = universal_function("Oktober_2022_Langenfeld",
+                                 pivot = TRUE,
+                                 startdate = "2022-09-30",
+                                 enddate = "2022-11-01")[[1]]
+
+
+# pick mean(min),mean(max) and mean Kalenborn
+
+K_max = round(mean(Kalenborn_val$Tmax), digits =2)
+Kmin = round(mean(Kalenborn_val$Tmin), digits = 2)
+Kmittel = round(mean(Kalenborn_val$Mittel), digits = 2)
+
+# pick mean(min),mean(max) and mean Langenfeld
+L_max = round(mean(Langenfeld_val$Tmax), digits =2)
+Lmin = round(mean(Langenfeld_val$Tmin), digits = 2)
+Lmittel = round(mean(Langenfeld_val$Mittel), digits = 2)
 
 
 
-Kalenborn$Ort = rep("Kalenborn", nrow(Kalenborn[, 1]))
-Langenfeld$Ort = rep("Langenfeld", nrow(Langenfeld[, 1]))
+# prepare df 
+Kalenborn$Ort = rep(paste0("Kalenborn-Scheuern"," ","Tmin ="," ",Kmin," ", "Tmax ="," ",K_max," ", "Tmean =", " ",Kmittel), nrow(Kalenborn[, 1]))
+Langenfeld$Ort = rep(paste0("Langenfeld"," ","Tmin ="," ",Lmin," ", "Tmax ="," ",L_max," ", "Tmean =", " ",Lmittel), nrow(Langenfeld[, 1]))
 df = rbind(Langenfeld,Kalenborn)
-
+tail(df)
 # visualization 
 require(reshape2)
-
 
 
 #dir.create("October_2022_result")
@@ -33,9 +56,11 @@ ggplot(data = df, aes(x = df$Tag, y = df$Temperature, groupe=Ort))+
   #facet_grid(cols=vars(Ort))+
   facet_wrap(~Ort, ncol = 1)+
   theme_gray(base_size = 15)+
-  labs(x="Tag", y = "Temperatur (°C)")+
+  labs(x="Tag", y = "Temperatur")+
   ggtitle("Oktober 2022")+
-  scale_x_continuous(breaks = seq(1,31,1))+
-  ggsave(file="October_2022_result/Oktober_2022.pdf", units ="cm",width=50, height=30, dpi=2000)
+  scale_x_continuous(breaks = seq(1,31,1))
+  #ggsave(file="October_2022_result/Oktober_2022.pdf", units ="cm",width=50, height=30, dpi=2000)
+  
 
- ?facet_grid()
+
+
